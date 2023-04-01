af4e4e3196124f7a8c95fc9a317a40f045782198
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2023-04-02 02:47:01 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✨ ʏᴛ ᴄʜᴀɴɴᴇʟ ✨", url=config.SUPPORT_CHANNEL
+                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.SUPPORT_CHANNEL
             )
         ],
      ]
